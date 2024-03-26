e1908d6c8bd2c96f3d181739f25e71b7922b99ae
Jasper Jan Tan <jasperjantan1996@gmail.com>
2024-03-26 00:16:40 -0400

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const Dotenv = require('dotenv-webpack');
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "micro-data2-1yqaii8n7-jaspers-projects-f11df7d9.vercel.app",
+    publicPath: "https://micro-data2-377fl2y03-jaspers-projects-f11df7d9.vercel.app/",
   },
 
   resolve: {
@@ -44,7 +44,7 @@ module.exports = (_, argv) => ({
       name: "StudentApp2",
       filename: "remoteEntry.js",
       remotes: {
-        studentapp1: "StudentApp1@http://localhost:4050/remoteEntry.js",
+        studentapp1: "StudentApp1@https://micro-data1-jaspers-projects-f11df7d9.vercel.app/remoteEntry.js",
       },
       exposes: {},
       shared: {
